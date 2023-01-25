a8dd92020d578b75a49643c4a0d408fbc70e48a2
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2023-01-25 19:27:34 +0530

    new

--- src/components/form-old/PartNineTen.js
+++ src/components/form-old/PartNineTen.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { useState } from "react";
-import { useFormContext } from "react-hook-form";
-import Form from "react-bootstrap/Form";
-import Accordion from "react-bootstrap/Accordion";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { Table } from "react-bootstrap";
+import React from 'react';
+import { useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+import Form from 'react-bootstrap/Form';
+import Accordion from 'react-bootstrap/Accordion';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Table } from 'react-bootstrap';
 
 const PartNineTen = () => {
   const {
@@ -21,90 +21,108 @@ const PartNineTen = () => {
     setPart10(!part10);
   };
 
+  const [patent, setPatent] = useState(['set']);
+  const addPatent = () => {
+    setPatent([...patent, 'set']);
+  };
   return (
     <>
-      <Accordion.Item eventKey="9">
+      <Accordion.Item eventKey='9'>
         <Accordion.Header>
           9. Patent Details (in last three years)
         </Accordion.Header>
         <Accordion.Body>
           <Form.Check
-            type="checkbox"
-            label="if Yes?"
+            type='checkbox'
+            label='if Yes?'
             onClick={handlePart9}
           ></Form.Check>
-          <Row className="mb-3">
-            <Form.Group
-              as={Col}
-              md="3"
-              disabled={part9}
-              controlId="patent_details"
-            >
-              <Form.Label>Patent Details</Form.Label>
-              <Form.Control
-                size="sm"
-                as="textarea"
-                rows={1}
-                disabled={part9}
-                {...register("patent_details", {
-                  maxLength: 500,
-                })}
-              />
-              {errors.patent_details && (
-                <p style={{ color: "red" }}>Please enter the patent details</p>
-              )}
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="patent_year">
-              <Form.Label>Year</Form.Label>
-              <Form.Control
-                size="sm"
-                type="text"
-                rows={3}
-                disabled={part9}
-                {...register("patent_year", {
-                  maxLength: 500,
-                })}
-              />
-              {errors.patent_year && (
-                <p style={{ color: "red" }}>Please enter the patented year</p>
-              )}
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="patent_status">
-              <Form.Label>Patent Status</Form.Label>
-              <Form.Select
-                size="sm"
-                type="text"
-                rows={3}
-                disabled={part9}
-                {...register("patent_status", {
-                  required: "Please select",
-                })}
-              >
-                <option selected="" disabled="" value="">
-                  Choose...
-                </option>
-                <option>Filled</option>
-                <option>Granted</option>
-              </Form.Select>
-              {errors.patent_status && (
-                <p style={{ color: "red" }}> {errors.patent_status.message}</p>
-              )}
-            </Form.Group>
-          </Row>
+          {patent.map(() => {
+            return (
+              <Row className='mb-3'>
+                <Form.Group
+                  as={Col}
+                  md='3'
+                  disabled={part9}
+                  controlId='patent_details'
+                >
+                  <Form.Label>Patent Details</Form.Label>
+                  <Form.Control
+                    size='sm'
+                    as='textarea'
+                    rows={1}
+                    disabled={part9}
+                    {...register('patent_details', {
+                      maxLength: 500,
+                    })}
+                  />
+                  {errors.patent_details && (
+                    <p style={{ color: 'red' }}>
+                      Please enter the patent details
+                    </p>
+                  )}
+                </Form.Group>
+                <Form.Group as={Col} md='3' controlId='patent_year'>
+                  <Form.Label>Year</Form.Label>
+                  <Form.Control
+                    size='sm'
+                    type='text'
+                    rows={3}
+                    disabled={part9}
+                    {...register('patent_year', {
+                      maxLength: 500,
+                    })}
+                  />
+                  {errors.patent_year && (
+                    <p style={{ color: 'red' }}>
+                      Please enter the patented year
+                    </p>
+                  )}
+                </Form.Group>
+                <Form.Group as={Col} md='3' controlId='patent_status'>
+                  <Form.Label>Patent Status</Form.Label>
+                  <Form.Select
+                    size='sm'
+                    type='text'
+                    rows={3}
+                    disabled={part9}
+                    {...register('patent_status', {
+                      required: 'Please select',
+                    })}
+                  >
+                    <option selected='' disabled='' value=''>
+                      Choose...
+                    </option>
+                    <option>Filled</option>
+                    <option>Granted</option>
+                  </Form.Select>
+                  {errors.patent_status && (
+                    <p style={{ color: 'red' }}>
+                      {' '}
+                      {errors.patent_status.message}
+                    </p>
+                  )}
+                </Form.Group>
+              </Row>
+            );
+          })}
+          <button type='button' onClick={addPatent} className='add-more-btn'>
+            Add
+          </button>
         </Accordion.Body>
       </Accordion.Item>
-      <Accordion.Item eventKey="10">
+      <Accordion.Item eventKey='10'>
         <Accordion.Header>
           10. Consultancy (in last three years)
         </Accordion.Header>
         <Accordion.Body>
           <Form.Check
-            type="checkbox"
-            label="if Yes?"
+            type='checkbox'
+            label='if Yes?'
             onClick={handlePart10}
           ></Form.Check>
           <Form.Group>
-            <Table responsive="sm">
+            <Table responsive='sm'>
               <thead>
                 <td>#</td>
                 <td>No. assignments</td>
@@ -115,16 +133,16 @@ const PartNineTen = () => {
                   <td>Completed</td>
                   <td>
                     <Form.Control
-                      size="sm"
-                      type="text"
+                      size='sm'
+                      type='text'
                       rows={3}
                       disabled={part10}
                     />
                   </td>
                   <td>
                     <Form.Control
-                      size="sm"
-                      type="text"
+                      size='sm'
+                      type='text'
                       rows={3}
                       disabled={part10}
                     />
@@ -134,16 +152,16 @@ const PartNineTen = () => {
                   <td>ongoing</td>
                   <td>
                     <Form.Control
-                      size="sm"
-                      type="text"
+                      size='sm'
+                      type='text'
                       rows={3}
                       disabled={part10}
                     />
                   </td>
                   <td>
                     <Form.Control
-                      size="sm"
-                      type="text"
+                      size='sm'
+                      type='text'
                       rows={3}
                       disabled={part10}
                     />
--- src/components/form-old/PartTwelve.js
+++ src/components/form-old/PartTwelve.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { useState } from "react";
-import { useFormContext } from "react-hook-form";
-import Form from "react-bootstrap/Form";
-import Accordion from "react-bootstrap/Accordion";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import React from 'react';
+import { useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+import Form from 'react-bootstrap/Form';
+import Accordion from 'react-bootstrap/Accordion';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const PartTwelve = () => {
   const {
@@ -21,11 +21,15 @@ const PartTwelve = () => {
   const interviewedHandle = () => {
     setInterviewed(!interviewed);
   };
+  const [know, setKnow] = useState(['set']);
+  const addKnow = () => {
+    setKnow([...know, 'set']);
+  };
   return (
-    <Accordion.Item eventKey="12">
+    <Accordion.Item eventKey='12'>
       <Accordion.Header>12. Other Information</Accordion.Header>
       <Accordion.Body>
-        <Row className="mb-3">
+        <Row className='mb-3'>
           {/* <Form.Group
             as={Col}
             md="12"
@@ -54,14 +58,13 @@ const PartTwelve = () => {
               <p style={{ color: "red" }}>Please enter</p>
             )}
           </Form.Group> */}
-
-          <Form.Group as={Col} md="12">
+          <Form.Group as={Col} md='12'>
             <Form.Label>Do You Know anyone in MUJ ?</Form.Label>
             <Form.Check
-              type="checkbox"
-              label="Yes"
-              value={"Yes"}
-              name={"knowanyone"}
+              type='checkbox'
+              label='Yes'
+              value={'Yes'}
+              name={'knowanyone'}
               // {...register("knowanyone", {
               //   required: true,
               // })}
@@ -76,33 +79,53 @@ const PartTwelve = () => {
                 required: true,
               })}
             ></Form.Check> */}
-            {errors.knowanyone && <p style={{ color: "red" }}>Please enter</p>}
+            {errors.knowanyone && <p style={{ color: 'red' }}>Please enter</p>}
           </Form.Group>
+          <h3></h3>
+          {knowanyone &&
+            know.map(() => {
+              return (
+                <>
+                  <br />
+                  <Form.Group as={Col} md='3'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control size='sm' type='text' placeholder='Name' />
+                  </Form.Group>
+                  <Form.Group as={Col} md='3'>
+                    <Form.Label>Designation</Form.Label>
+                    <Form.Control
+                      size='sm'
+                      type='text'
+                      placeholder='Designation'
+                    />
+                  </Form.Group>
+                  <Form.Group as={Col} md='3'>
+                    <Form.Label>Department</Form.Label>
+                    <Form.Control
+                      size='sm'
+                      type='text'
+                      placeholder='Department'
+                    />
+                  </Form.Group>
+                  <h3></h3>
+                  <br />
+                </>
+              );
+            })}
           {knowanyone && (
-            <>
-              <Form.Group as={Col} md="3">
-                <Form.Label>Name</Form.Label>
-                <Form.Control size="sm" type="text" placeholder="Name" />
-              </Form.Group>
-              <Form.Group as={Col} md="3">
-                <Form.Label>Designation</Form.Label>
-                <Form.Control size="sm" type="text" placeholder="Designation" />
-              </Form.Group>
-              <Form.Group as={Col} md="3">
-                <Form.Label>Department</Form.Label>
-                <Form.Control size="sm" type="text" placeholder="Department" />
-              </Form.Group>
-            </>
+            <button type='button' onClick={addKnow} className='add-more-btn'>
+              Add
+            </button>
           )}
-          <Form.Group as={Col} md="12">
+          <Form.Group as={Col} md='12'>
             <Form.Label>
               Have you ever been interviewed in MUJ earlier?
             </Form.Label>
             <Form.Check
-              type="checkbox"
-              label="Yes?"
-              value={"Yes"}
-              name={"knowanyone"}
+              type='checkbox'
+              label='Yes?'
+              value={'Yes'}
+              name={'knowanyone'}
               // {...register("knowanyone", {
               //   required: true,
               // })}
@@ -111,111 +134,111 @@ const PartTwelve = () => {
           </Form.Group>
           {interviewed && (
             <>
-              <Form.Group as={Col} md="12">
+              <Form.Group as={Col} md='12'>
                 <Form.Label>Given Offer to join?</Form.Label>
                 <Form.Check
-                  type="radio"
-                  label="Yes"
-                  value={"Yes"}
-                  name={"offerjoined"}
-                  {...register("offerjoined", {
+                  type='radio'
+                  label='Yes'
+                  value={'Yes'}
+                  name={'offerjoined'}
+                  {...register('offerjoined', {
                     required: true,
                   })}
                 ></Form.Check>
                 <Form.Check
-                  type="radio"
-                  label="No"
-                  value={"No"}
-                  name={"offerjoined"}
-                  {...register("offerjoined", {
+                  type='radio'
+                  label='No'
+                  value={'No'}
+                  name={'offerjoined'}
+                  {...register('offerjoined', {
                     required: true,
                   })}
                 ></Form.Check>
                 {errors.offerjoined && (
-                  <p style={{ color: "red" }}>Please enter</p>
+                  <p style={{ color: 'red' }}>Please enter</p>
                 )}
               </Form.Group>
-              <Form.Group as={Col} md="12">
+              <Form.Group as={Col} md='12'>
                 <Form.Label>Joined?</Form.Label>
                 <Form.Check
-                  type="radio"
-                  label="Yes"
-                  value={"Yes"}
-                  name={"joined"}
+                  type='radio'
+                  label='Yes'
+                  value={'Yes'}
+                  name={'joined'}
                 ></Form.Check>
                 <Form.Check
-                  type="radio"
-                  label="No"
-                  value={"No"}
-                  name={"joined"}
+                  type='radio'
+                  label='No'
+                  value={'No'}
+                  name={'joined'}
                 ></Form.Check>
               </Form.Group>
             </>
           )}
-          <Form.Group as={Col} md="12">
+          <Form.Group as={Col} md='12'>
             <Form.Label>
               Have you ever been punished during your service or convicted by a
               court of law?
             </Form.Label>
             <Form.Check
-              type="radio"
-              label="Yes"
-              value={"Yes"}
-              name={"convicted"}
-              {...register("convicted", {
+              type='radio'
+              label='Yes'
+              value={'Yes'}
+              name={'convicted'}
+              {...register('convicted', {
                 required: true,
               })}
             ></Form.Check>
             <Form.Check
-              type="radio"
-              label="No"
-              value={"No"}
-              name={"convicted"}
-              {...register("convicted", {
+              type='radio'
+              label='No'
+              value={'No'}
+              name={'convicted'}
+              {...register('convicted', {
                 required: true,
               })}
             ></Form.Check>
-            {errors.convicted && <p style={{ color: "red" }}>Please enter</p>}
+            {errors.convicted && <p style={{ color: 'red' }}>Please enter</p>}
           </Form.Group>
-          <Form.Group as={Col} md="12">
+          <Form.Group as={Col} md='12'>
             <Form.Label>
               <u>is Yes, Please Explain</u>
             </Form.Label>
-            <Form.Control size="sm" type="text" placeholder="Explain" />
+            <Form.Control size='sm' type='text' placeholder='Explain' />
           </Form.Group>
-          <Form.Group as={Col} md="12">
+          <Form.Group as={Col} md='12'>
             <Form.Label>
               Do you have any case pending against you in court of law?
             </Form.Label>
             <Form.Check
-              type="radio"
-              label="Yes"
-              value={"Yes"}
-              name={"pendingcases"}
-              id={"pendingcases"}
-              {...register("pendingcases", {
+              type='radio'
+              label='Yes'
+              value={'Yes'}
+              name={'pendingcases'}
+              id={'pendingcases'}
+              {...register('pendingcases', {
                 required: true,
               })}
             ></Form.Check>
             <Form.Check
-              type="radio"
-              label="No"
-              value={"No"}
-              name={"pendingcases"}
-              id={"pendingcases"}
-              {...register("pendingcases", {
+              type='radio'
+              label='No'
+              value={'No'}
+              name={'pendingcases'}
+              id={'pendingcases'}
+              {...register('pendingcases', {
                 required: true,
               })}
             ></Form.Check>
             {errors.pendingcases && (
-              <p style={{ color: "red" }}>Please enter</p>
+              <p style={{ color: 'red' }}>Please enter</p>
             )}
           </Form.Group>
-          <Form.Group as={Col} md="12">
+          <Form.Group as={Col} md='12'>
             <Form.Label>
               <u>is Yes, Please Explain</u>
             </Form.Label>
-            <Form.Control size="sm" type="text" placeholder="Explain" />
+            <Form.Control size='sm' type='text' placeholder='Explain' />
           </Form.Group>
         </Row>
       </Accordion.Body>
