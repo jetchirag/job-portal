47ff18690af2f0423f7f5b051387401e5aee7180
Chirag Aggarwal <thechiragaggarwal@gmail.com>
2023-02-12 22:17:20 +0530

    Blob for download incoming stream as file for csv download

--- src/components/admin/applicationtable.js
+++ src/components/admin/applicationtable.js
@@ -257,9 +257,18 @@ const ApplicationsTable = ({ isLoading }) => {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
         },
+        responseType: "blob",
       })
       .then((response) => {
+        console.log("Downloading");
         // console.log(response.data);
+        const csvData = new Blob([response.data], { type: "text/csv" });
+        const csvURL = window.URL.createObjectURL(csvData);
+        const tempLink = document.createElement("a");
+        tempLink.href = csvURL;
+        tempLink.setAttribute("download", "data.csv");
+        tempLink.click();
+
       })
       .catch((err) => {
         console.log(err);
