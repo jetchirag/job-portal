db6df7b57676cca0f4cb81998a8dfbf358e6fb91
Somesh Kumar <kumarsomesh.unos@gmail.com>
2023-02-25 00:49:28 +0530

    change carousel size

--- src/components/WhyUS2.js
+++ src/components/WhyUS2.js
@@ -5,11 +5,11 @@ const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 5,
+    items: 6,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
+    items: 4,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
@@ -21,276 +21,224 @@ const responsive = {
   },
 };
 const WhyUS2 = () => {
+  const cardStyle = {
+    width: "22rem",
+    height: "auto",
+    marginBottom: "3rem"
+  }
   return (
     <Carousel
-      swipeable={false}
+      swipeable={true}
       draggable={false}
       showDots={true}
       responsive={responsive}
       infinite={true}
     >
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/19.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/12.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/14.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/15.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/17.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/18.png' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/20.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/21.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/22.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/23.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/7.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/8.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/9.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/10.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/1.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/2.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/3.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/4.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
-      <div className='card why-cards'>
+      <div className='card why-cards' style={cardStyle} >
         <div className='card-img'>
           <img src='https://mujbioscope.in/assets/collage/6.jpg' alt='' />
         </div>
-        <div className='card-container'>
+        {/* <div className='card-container'>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
-            natus molestias quas sint, asperiores dolore error hic fugit
-            inventore quos placeat rerum, necessitatibus ducimus saepe, suscipit
-            sed itaque blanditiis labore!
+            
           </p>
-        </div>
+        </div> */}
       </div>
     </Carousel>
   );
