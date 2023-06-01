ebd7e7985cc005cc427a17adf4313326adc54a00
Rajit <73546886+rajitk13@users.noreply.github.com>
2023-06-01 10:50:30 +0530

    Partone Restructured

--- src/components/Form-Current/PartOne.js
+++ src/components/Form-Current/PartOne.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import { useState } from 'react';
-import { useFormContext } from 'react-hook-form';
-import Form from 'react-bootstrap/Form';
-import Accordion from 'react-bootstrap/Accordion';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-import { facultiesList, departmentList2 } from './faculties';
+import React from "react";
+import { useState } from "react";
+import { useFormContext } from "react-hook-form";
+import Form from "react-bootstrap/Form";
+import Accordion from "react-bootstrap/Accordion";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Data from "../Department.json";
+// import { facultiesList, departmentList2 } from './faculties';
 
 const Partone = () => {
-  const [jobType, setJobType] = useState('');
-  const [faculty, setFaculty] = useState('');
-  const [school, setSchool] = useState('');
+  const [dept, setDept] = useState("");
+  // const [faculty, setFaculty] = useState('');
+  // const [school, setSchool] = useState('');
 
-  const handleJobTypeChange = (event) => {
-    setJobType(event.target.value);
-    if (event.target.value === 'non_academic') {
-      setFaculty('NA');
-      setSchool('NA');
-    }
-    else {
-      setFaculty('');
-      setSchool('');
-    }
-  };
+  // const handleJobTypeChange = (event) => {
+  //   setJobType(event.target.value);
+  //   if (event.target.value === 'non_academic') {
+  //     setFaculty('NA');
+  //     setSchool('NA');
+  //   }
+  //   else {
+  //     setFaculty('');
+  //     setSchool('');
+  //   }
+  // };
 
   const {
     register,
     formState: { errors },
   } = useFormContext();
-  const facultyHandler = (e) => {
-    setFaculty(e.target.value);
-    setSchool('');
-  };
-  const schoolHandler = (e) => {
-    setSchool(e.target.value);
+  // const facultyHandler = (e) => {
+  //   setFaculty(e.target.value);
+  //   setSchool('');
+  // };
+  const deptHandler = (e) => {
+    setDept(e.target.value);
   };
 
   return (
-    <Accordion.Item eventKey='0' styles={{ height: '1rem !important' }}>
+    <Accordion.Item eventKey="0" styles={{ height: "1rem !important" }}>
       <Accordion.Header>1. Application Information</Accordion.Header>
       <Accordion.Body>
-        <Row className='mb-3'>
-          <Form.Group as={Col} md='2' controlId='jobtype'>
+        <Row className="mb-3">
+          {/* <Form.Group as={Col} md='2' controlId='jobtype'>
             <Form.Label>
               Job Type<span style={{ color: 'red' }}> *</span>
             </Form.Label>
@@ -55,101 +55,66 @@ const Partone = () => {
                 required: true,
               })}
               onChange={handleJobTypeChange}
-            >
-              <option selected='' disabled='' value=''>
-                Choose...
-              </option>
-              <option value='academic'>Academic</option>
-              <option value='non_academic'>Non Academic</option>
-              {/* <option value="administration">Administrative</option> */}
-            </Form.Select>
-          </Form.Group>
-          
-          {jobType === 'academic' && (
-          <Form.Group as={Col} md='2' controlId='faculty'>
+            > */}
+          {/* <option selected='' disabled='' value=''> */}
+          {/* Choose... */}
+          {/* </option> */}
+          {/* <option value='academic'>Academic</option> */}
+          {/* <option value='non_academic'>Non Academic</option> */}
+          {/* <option value="administration">Administrative</option> */}
+          {/* </Form.Select> */}
+          {/* </Form.Group> */}
+
+          <Form.Group as={Col} md="2" controlId="deparment">
             <Form.Label>
-              Faculty<span style={{ color: 'red' }}> *</span>
+              Department<span style={{ color: "red" }}> *</span>
             </Form.Label>
             <Form.Select
-              isInvalid={errors.faculty && faculty===''}
-              size='sm'
-              aria-label='Default Faculty Type'
-              {...register('faculty', {
+              isInvalid={errors.department}
+              size="sm"
+              aria-label="Default Department Type"
+              {...register("department", {
                 required: true,
               })}
-              placeholder='acad'
-              onChange={facultyHandler}
+              placeholder="acad"
+              onChange={deptHandler}
             >
-              <option selected='' disabled='' value=''>
+              <option selected="" disabled="" value="">
                 Choose...
               </option>
 
-              {Object.keys(facultiesList).map((faculty) => {
-                return <option value={faculty}>{faculty}</option>;
+              {Object.keys(Data).map((dept) => {
+                return <option value={dept}>{dept}</option>;
               })}
             </Form.Select>
           </Form.Group>
-          )}
-          {jobType === 'academic' && faculty && (
-            <Form.Group as={Col} md='2' controlId='school'>
-              <Form.Label>
-                School<span style={{ color: 'red' }}> *</span>
-              </Form.Label>
-              <Form.Select
-                isInvalid={errors.school && school===''}
-                size='sm'
-                aria-label='Default Faculty Type'
-                {...register('school', {
-                  required: true,
-                })}
-                onChange={schoolHandler}
-              >
-                <option selected='' disabled='' value=''>
-                  Choose...
-                </option>
 
-                {faculty &&
-                  Object.keys(facultiesList[faculty]).map((sch) => {
-                    return <option value={sch}>{sch}</option>;
-                  })}
-              </Form.Select>
-            </Form.Group>
-          )}
-          {school && (
-            <Form.Group as={Col} md='2' controlId='department'>
-              <Form.Label>
-                Department<span style={{ color: 'red' }}> *</span>
-              </Form.Label>
-              <Form.Select
-                isInvalid={errors.dept}
-                size='sm'
-                aria-label='Default Faculty Type'
-                {...register('department', {
-                  required: true,
-                })}
-              >
-                <option selected='' disabled='' value=''>
-                  Choose...
-                </option>
-                {school &&
-                  (school === 'NA' ? (
-                    // Custom department list when school is "NA"
-                    departmentList2.map((dept) => (
-                      <option key={dept} value={dept}>
-                        {dept}
-                      </option>
-                    ))
-                  ) : (
-                    // Regular department list based on facultiesList
-                    facultiesList[faculty][school].map((dept) => (
-                      <option key={dept} value={dept}>
-                        {dept}
-                      </option>
-                    ))
-                  ))}
-              </Form.Select>
-            </Form.Group>
-          )}
+          <Form.Group as={Col} md="2" controlId="school">
+            <Form.Label>
+              School<span style={{ color: "red" }}> *</span>
+            </Form.Label>
+            <Form.Select size="sm" aria-label="Default School Type" disabled={true}>
+              <option selected="" disabled="" value="">
+                {dept&&Object.keys(Data[dept])}
+              </option>
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group as={Col} md="2" controlId="department">
+            <Form.Label>
+              Faculty<span style={{ color: "red" }}> *</span>
+            </Form.Label>
+            <Form.Select
+              size="sm"
+              aria-label="Default Faculty Type"
+              disabled={true}
+            >
+              <option selected="" disabled="" value="">
+                {dept&& Object.keys(Data[dept][Object.keys(Data[dept])[0]])}
+              </option>
+            </Form.Select>
+          </Form.Group>
+
           {/* <Form.Group as={Col} md="2" controlId="faculty">
             <Form.Label>
               Nature of Job<span style={{ color: "red" }}> *</span>
