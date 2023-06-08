0241c502442683c79f9c4aa8c549365c25397575
Somesh Kumar <kumarsomesh.unos@gmail.com>
2023-06-08 13:41:02 +0530

    add type of job in experiance

--- src/components/Form-Current/PartFive.js
+++ src/components/Form-Current/PartFive.js
@@ -64,7 +64,7 @@ const PartFive = () => {
   };
 
   return (
-    <Accordion.Item eventKey='4'>
+    <Accordion.Item eventKey="4">
       <Accordion.Header>5. Experience</Accordion.Header>
       <Accordion.Body>
         <p>
@@ -72,21 +72,21 @@ const PartFive = () => {
         </p>
         {acad.map(() => {
           return (
-            <Row className='mb-3'>
+            <Row className="mb-3">
               {/* Nature of job  */}
-              <Form.Group as={Col} md='2' controlId='academicExperience_mode'>
+              <Form.Group as={Col} md="2" controlId="academicExperience_mode">
                 <Form.Label>Nature of Job</Form.Label>
                 <Form.Select
-                  size='sm'
+                  size="sm"
                   {...register("academicExperience_mode", {
                     // required: "Please select the nature of your job",
                   })}
                 >
-                  <option selected='' disabled='' value=''>
+                  <option selected="" disabled="" value="">
                     Choose...
                   </option>
-                  <option value='regular'>Regular</option>
-                  <option value='part-time'>Part-Time</option>
+                  <option value="regular">Regular</option>
+                  <option value="part-time">Part-Time</option>
                 </Form.Select>
                 {errors.academicExperience_mode && (
                   <p style={{ color: "red" }}>
@@ -98,18 +98,18 @@ const PartFive = () => {
               {/* Country of job  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='academicExperience_country'
+                md="2"
+                controlId="academicExperience_country"
               >
                 <Form.Label>Country</Form.Label>
                 <Form.Select
                   isInvalid={errors.academicExperience_country}
-                  size='sm'
+                  size="sm"
                   {...register("academicExperience_country", {
                     required: false,
                   })}
                 >
-                  <option selected='' disabled='' value=''>
+                  <option selected="" disabled="" value="">
                     Choose...
                   </option>
                   {countries?.map((element) => {
@@ -119,48 +119,44 @@ const PartFive = () => {
               </Form.Group>
 
               {/* State Details  */}
-              <Form.Group as={Col} md='2' controlId='academicExperience_state'>
-                <Form.Label>
-                  State
-                </Form.Label>
+              <Form.Group as={Col} md="2" controlId="academicExperience_state">
+                <Form.Label>State</Form.Label>
 
                 <Form.Control
-              isInvalid={errors.academicExperience_state}
-              size='sm'
-              placeholder='State'
-              type='text'
-              {...register("academicExperience_state", {
-                required: false,
-              })}
-            />
+                  isInvalid={errors.academicExperience_state}
+                  size="sm"
+                  placeholder="State"
+                  type="text"
+                  {...register("academicExperience_state", {
+                    required: false,
+                  })}
+                />
               </Form.Group>
               {/* City Details  */}
-              <Form.Group as={Col} md='2' controlId='academicExperience_city'>
-                <Form.Label>
-                  City
-                </Form.Label>
+              <Form.Group as={Col} md="2" controlId="academicExperience_city">
+                <Form.Label>City</Form.Label>
                 <Form.Control
-              isInvalid={errors.academicExperience_city}
-              size='sm'
-              placeholder='City'
-              type='text'
-              {...register("academicExperience_city", {
-                required: false,
-              })}
-            />
+                  isInvalid={errors.academicExperience_city}
+                  size="sm"
+                  placeholder="City"
+                  type="text"
+                  {...register("academicExperience_city", {
+                    required: false,
+                  })}
+                />
               </Form.Group>
 
               {/* University  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='academicExperience_university'
+                md="2"
+                controlId="academicExperience_university"
               >
                 <Form.Label>University</Form.Label>
                 <Form.Control
-                  size='sm'
-                  placeholder='University'
-                  type='text'
+                  size="sm"
+                  placeholder="University"
+                  type="text"
                   {...register("academicExperience_university", {
                     // required: true,
                     maxLength: 100,
@@ -170,14 +166,14 @@ const PartFive = () => {
               {/* College Name  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='academicExperience_college'
+                md="2"
+                controlId="academicExperience_college"
               >
                 <Form.Label>College</Form.Label>
                 <Form.Control
-                  size='sm'
-                  placeholder='College Name'
-                  type='text'
+                  size="sm"
+                  placeholder="College Name"
+                  type="text"
                   {...register("academicExperience_college", {
                     // required: true,
                     maxLength: 100,
@@ -187,36 +183,36 @@ const PartFive = () => {
               {/* Postion of work  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='academicExperience_position'
+                md="2"
+                controlId="academicExperience_position"
               >
                 <Form.Label>Position</Form.Label>
                 <Form.Select
-                  size='sm'
+                  size="sm"
                   {...register("academicExperience_position", {
                     // required: 'Please select the position you worked in',
                   })}
                 >
-                  <option selected='' disabled='' value=''>
+                  <option selected="" disabled="" value="">
                     Choose...
                   </option>
-                  <option value='professor'>Professor</option>
-                  <option value='associate-professor'>
+                  <option value="professor">Professor</option>
+                  <option value="associate-professor">
                     Associate Professor
                   </option>
-                  <option value='assistant-professor'>
+                  <option value="assistant-professor">
                     Assistant Professor
                   </option>
-                  <option value='research-assistant'>Research Assistant</option>
+                  <option value="research-assistant">Research Assistant</option>
                 </Form.Select>
               </Form.Group>
               {/* Academic Domain Details  */}
-              <Form.Group as={Col} md='2' controlId='academicExperience_domain'>
+              <Form.Group as={Col} md="2" controlId="academicExperience_domain">
                 <Form.Label>Academic Domain</Form.Label>
                 <Form.Control
-                  size='sm'
-                  placeholder='Academic Domain'
-                  type='text'
+                  size="sm"
+                  placeholder="Academic Domain"
+                  type="text"
                   {...register("academicExperience_domain", {
                     // required: true,
                     maxLength: 100,
@@ -226,14 +222,14 @@ const PartFive = () => {
               {/* Working From Date Details  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='academicExperience_datefrom'
+                md="2"
+                controlId="academicExperience_datefrom"
               >
                 <Form.Label>Date From</Form.Label>
                 <Form.Control
-                  size='sm'
-                  placeholder='DD/MM/YYYY'
-                  type='date'
+                  size="sm"
+                  placeholder="DD/MM/YYYY"
+                  type="date"
                   {...register("academicExperience_datefrom")}
                 />
                 {errors.academicExperience_datefrom && (
@@ -243,12 +239,12 @@ const PartFive = () => {
                 )}
               </Form.Group>
               {/* Working To Date Details  */}
-              <Form.Group as={Col} md='2' controlId='academicExperience_dateto'>
+              <Form.Group as={Col} md="2" controlId="academicExperience_dateto">
                 <Form.Label>Date To</Form.Label>
                 <Form.Control
-                  size='sm'
-                  placeholder='DD/MM/YYYY'
-                  type='date'
+                  size="sm"
+                  placeholder="DD/MM/YYYY"
+                  type="date"
                   {...register("academicExperience_dateto")}
                 />
                 {errors.academicExperience_dateto && (
@@ -261,16 +257,16 @@ const PartFive = () => {
           );
         })}
         <button
-          type='button'
+          type="button"
           onClick={addAcad}
-          className='btn btn-outline-success'
+          className="btn btn-outline-success"
         >
           Add New
         </button>{" "}
         <button
-          type='button'
+          type="button"
           onClick={removeAcad}
-          className='btn btn-outline-danger'
+          className="btn btn-outline-danger"
         >
           Remove Last
         </button>
@@ -280,42 +276,64 @@ const PartFive = () => {
         </p>
         {nonAcad.map(() => {
           return (
-            <Row className='mb-3'>
+            <Row className="mb-3">
+              {/* Type of job  */}
+              <Form.Group as={Col} md="2" controlId="academicExperience_type">
+                <Form.Label>Type of Job</Form.Label>
+                <Form.Select
+                  size="sm"
+                  {...register("academicExperience_type", {
+                    // required: "Please select the nature of your job",
+                  })}
+                >
+                  <option selected="" disabled="" value="">
+                    Choose...
+                  </option>
+                  <option value="administrative">Administrative</option>
+                  <option value="non-academic">Non-Academic</option>
+                </Form.Select>
+                {errors.academicExperience_type && (
+                  <p style={{ color: "red" }}>
+                    {" "}
+                    {errors.academicExperience_type.message}
+                  </p>
+                )}
+              </Form.Group>
               {/* Nature of job  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='nonAcademicExperience_mode'
+                md="2"
+                controlId="nonAcademicExperience_mode"
               >
                 <Form.Label>Nature of Job</Form.Label>
                 <Form.Select
-                  size='sm'
+                  size="sm"
                   {...register("nonAcademicExperience_mode", {
                     // required: 'Please select the nature of your job',
                   })}
                 >
-                  <option selected='' disabled='' value=''>
+                  <option selected="" disabled="" value="">
                     Choose...
                   </option>
-                  <option value='regular'>Regular</option>
-                  <option value='part-time'>Part-Time</option>
+                  <option value="regular">Regular</option>
+                  <option value="part-time">Part-Time</option>
                 </Form.Select>
               </Form.Group>
               {/* Country of job  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='nonAcademicExperience_country'
+                md="2"
+                controlId="nonAcademicExperience_country"
               >
                 <Form.Label>Country</Form.Label>
                 <Form.Select
                   isInvalid={errors.nonAcademicExperience_country}
-                  size='sm'
+                  size="sm"
                   {...register("nonAcademicExperience_country", {
                     required: false,
                   })}
                 >
-                  <option selected='' disabled='' value=''>
+                  <option selected="" disabled="" value="">
                     Choose...
                   </option>
                   {countries?.map((element) => {
@@ -325,48 +343,52 @@ const PartFive = () => {
               </Form.Group>
 
               {/* State Details  */}
-              <Form.Group as={Col} md='2' controlId='nonAcademicExperience_state'>
-                <Form.Label>
-                  State
-                </Form.Label>
+              <Form.Group
+                as={Col}
+                md="2"
+                controlId="nonAcademicExperience_state"
+              >
+                <Form.Label>State</Form.Label>
 
                 <Form.Control
-              isInvalid={errors.nonAcademicExperience_state}
-              size='sm'
-              placeholder='State'
-              type='text'
-              {...register("nonAcademicExperience_state", {
-                required: false,
-              })}
-            />
+                  isInvalid={errors.nonAcademicExperience_state}
+                  size="sm"
+                  placeholder="State"
+                  type="text"
+                  {...register("nonAcademicExperience_state", {
+                    required: false,
+                  })}
+                />
               </Form.Group>
               {/* City Details  */}
-              <Form.Group as={Col} md='2' controlId='nonAcademicExperience_city'>
-                <Form.Label>
-                  City
-                </Form.Label>
+              <Form.Group
+                as={Col}
+                md="2"
+                controlId="nonAcademicExperience_city"
+              >
+                <Form.Label>City</Form.Label>
                 <Form.Control
-              isInvalid={errors.nonAcademicExperience_city}
-              size='sm'
-              placeholder='City'
-              type='text'
-              {...register("nonAcademicExperience_city", {
-                required: false,
-              })}
-            />
+                  isInvalid={errors.nonAcademicExperience_city}
+                  size="sm"
+                  placeholder="City"
+                  type="text"
+                  {...register("nonAcademicExperience_city", {
+                    required: false,
+                  })}
+                />
               </Form.Group>
 
               {/* Organization Name  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='nonAcademicExperience_organization'
+                md="2"
+                controlId="nonAcademicExperience_organization"
               >
                 <Form.Label>Organization</Form.Label>
                 <Form.Control
-                  size='sm'
-                  placeholder='Organization Name'
-                  type='text'
+                  size="sm"
+                  placeholder="Organization Name"
+                  type="text"
                   {...register("nonAcademicExperience_organization", {
                     // required: true,
                     maxLength: 100,
@@ -376,14 +398,14 @@ const PartFive = () => {
               {/* Designation Name  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='nonAcademicExperience_designation'
+                md="2"
+                controlId="nonAcademicExperience_designation"
               >
                 <Form.Label>Designation</Form.Label>
                 <Form.Control
-                  size='sm'
-                  placeholder='Designation Name'
-                  type='text'
+                  size="sm"
+                  placeholder="Designation Name"
+                  type="text"
                   {...register("nonAcademicExperience_designation", {
                     // required: true,
                     maxLength: 100,
@@ -393,14 +415,14 @@ const PartFive = () => {
               {/* Department Name  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='nonAcademicExperience_department'
+                md="2"
+                controlId="nonAcademicExperience_department"
               >
                 <Form.Label>Department</Form.Label>
                 <Form.Control
-                  size='sm'
-                  placeholder='Department Name'
-                  type='text'
+                  size="sm"
+                  placeholder="Department Name"
+                  type="text"
                   {...register("nonAcademicExperience_department", {
                     // required: true,
                     maxLength: 100,
@@ -410,28 +432,28 @@ const PartFive = () => {
               {/* Working From Date Details  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='nonAcademicExperience_datefrom'
+                md="2"
+                controlId="nonAcademicExperience_datefrom"
               >
                 <Form.Label>Date From</Form.Label>
                 <Form.Control
-                  size='sm'
-                  placeholder='DD/MM/YYYY'
-                  type='date'
+                  size="sm"
+                  placeholder="DD/MM/YYYY"
+                  type="date"
                   {...register("nonAcademicExperience_datefrom")}
                 />
               </Form.Group>
               {/* Working To Date Details  */}
               <Form.Group
                 as={Col}
-                md='2'
-                controlId='nonAcademicExperience_dateto'
+                md="2"
+                controlId="nonAcademicExperience_dateto"
               >
                 <Form.Label>Date To</Form.Label>
                 <Form.Control
-                  size='sm'
-                  placeholder='DD/MM/YYYY'
-                  type='date'
+                  size="sm"
+                  placeholder="DD/MM/YYYY"
+                  type="date"
                   {...register("nonAcademicExperience_dateto")}
                 />
                 {errors.nonAcademicExperience_dateto && (
@@ -444,16 +466,16 @@ const PartFive = () => {
           );
         })}
         <button
-          type='button'
+          type="button"
           onClick={addNonAcad}
-          className='btn btn-outline-success'
+          className="btn btn-outline-success"
         >
           Add New
         </button>{" "}
         <button
-          type='button'
+          type="button"
           onClick={removeNonAcad}
-          className='btn btn-outline-danger'
+          className="btn btn-outline-danger"
         >
           Remove Last
         </button>
